81e02e481245bee7b60d6531432c25b4eb5d7b24
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-07-17 08:12:58 -0700

    suit parse

--- src/card/rank.rs
+++ src/card/rank.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 use thiserror::Error;
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+#[derive(PartialEq, Eq, Debug)]
 pub enum Rank {
     Two,
     Three,
@@ -26,23 +26,6 @@ pub struct RankParseError(String);
 impl FromStr for Rank {
     type Err = RankParseError;
 
-    /// Parses a card rank like the following:
-    /// ```
-    /// use degen::card::rank::Rank;
-    /// assert_eq!("A".parse::<Rank>().unwrap(), Rank::Ace);
-    /// assert_eq!("K".parse::<Rank>().unwrap(), Rank::King);
-    /// assert_eq!("Q".parse::<Rank>().unwrap(), Rank::Queen);
-    /// assert_eq!("J".parse::<Rank>().unwrap(), Rank::Jack);
-    /// assert_eq!("10".parse::<Rank>().unwrap(), Rank::Ten);
-    /// assert_eq!("9".parse::<Rank>().unwrap(), Rank::Nine);
-    /// assert_eq!("8".parse::<Rank>().unwrap(), Rank::Eight);
-    /// assert_eq!("7".parse::<Rank>().unwrap(), Rank::Seven);
-    /// assert_eq!("6".parse::<Rank>().unwrap(), Rank::Six);
-    /// assert_eq!("5".parse::<Rank>().unwrap(), Rank::Five);
-    /// assert_eq!("4".parse::<Rank>().unwrap(), Rank::Four);
-    /// assert_eq!("3".parse::<Rank>().unwrap(), Rank::Three);
-    /// assert_eq!("2".parse::<Rank>().unwrap(), Rank::Two);
-    /// ```
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let rank = match s {
             "2" => Self::Two,
